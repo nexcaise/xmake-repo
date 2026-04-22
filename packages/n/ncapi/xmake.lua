28de1e3cfd7b7f6e39fd7cd7ffe33a2c78367c23
alvinqid <github@alvinq.my.id>
2026-04-22 12:49:11 +0700

    fix or test!

--- packages/n/ncapi/xmake.lua
+++ packages/n/ncapi/xmake.lua
@@ -2,15 +2,16 @@ package("ncapi")
     set_homepage("https://github.com/nexcaise/ncapi")
     set_description("The ncapi package")
     set_kind("library")
-
     add_urls("https://github.com/nexcaise/ncapi/archive/refs/tags/$(version).tar.gz")
-    add_versions("0.0.1", "a83f47eb385382c1fd3cd46d860cd7437017797f77f180abb147a3e8a9ba1a81")
-    add_versions("0.0.2-beta.1", "12ab078f59a7be5af9f9874838cc26859d88453811ac184d411d7f7c7520cee8")
+    
+    
+    add_versions("0.0.2-beta.3", "e3e0b3b48316e197cdaa0c1ab60763887309c19b18c92f24ef0959a22c096c27")
 
     add_deps("cmake")
+    add_configs("shared", {description = "Build shared library", default = false, type = "boolean"})
 
     on_load(function (package)
-        package:set("cmake.build_shared", true)
+        package:set("cmake.build_shared", package:config("shared"))
         package:add("includedirs", "include")
         package:add("linkdirs", "libs/arm64-v8a")
         package:add("links", "GlossHook", "nise", "log")
