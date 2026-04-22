890ce8159febff03b03a687061f6d119f1b42473
alvinqid <github@alvinq.my.id>
2026-04-22 12:34:50 +0700

    fix or test!

--- packages/n/ncapi/xmake.lua
+++ packages/n/ncapi/xmake.lua
@@ -5,6 +5,7 @@ package("ncapi")
 
     add_urls("https://github.com/nexcaise/ncapi/archive/refs/tags/$(version).tar.gz")
     add_versions("0.0.1", "a83f47eb385382c1fd3cd46d860cd7437017797f77f180abb147a3e8a9ba1a81")
+    add_versions("0.0.2-beta.1", "12ab078f59a7be5af9f9874838cc26859d88453811ac184d411d7f7c7520cee8")
 
     add_deps("cmake")
 
